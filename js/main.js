a4a8a68fdcd01e9c57d8888427232c240793c568
Tissebaos <tissebaostj@gmail.com>
2020-05-13 01:28:42 +0100

    finalisation numero 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /*-- main.js --*/
 /*-- By Togodba Tissébé Jean (Tissébaos) --*/
+
+function smaller_menu_display (id) {
+  if (document.getElementById(id).style.transform == "translateY(0%)") {
+    document.getElementById(id).style.transform = "translateY(-100%)"
+  } else {
+    document.getElementById(id).style.transform = "translateY(0%)"
+  }
+}
